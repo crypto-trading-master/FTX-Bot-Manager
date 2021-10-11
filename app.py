626334483854f422f76614620dd9565891d98eee
crypto-trading-master <trading_master@outlook.com>
2021-10-11 19:24:48 +0200

    Before deploying to dokku

--- app.py
+++ app.py
@@ -68,7 +68,52 @@ def ftx_top_pairs_spot():
                 tradeableMarkets.append(tradeableMarket)
 
     df = pd.DataFrame(tradeableMarkets)
-    df.sort_values(by='volumeM', inplace=True, ascending=False)    
+    df.sort_values(by='volatility', inplace=True, ascending=False)    
 
     return render_template('data.html',  tables=[df.to_html(classes='data', header="true")])
 
+@ app.route('/ftx-top-pairs-futures', methods=['GET'])
+def ftx_top_pairs_futures():
+
+    markets = client.get_futures()
+
+    tradeableMarkets = []  # type: list
+
+    endTime = int(time.time())
+    startTime = endTime - 86400 * 3
+
+    for market in markets:
+
+        bid = market["bid"]
+        ask = market["ask"]
+        volume = market["volumeUsd24h"]
+
+        if (market["enabled"] and 
+            volume > minVolume24h):
+            
+            name = market["name"]
+
+            hours = client.get_historical_data(market_name=name, resolution=3600, limit=72, start_time=startTime, end_time=endTime)
+
+            low = 0
+            high = 0
+
+            for hour in hours:
+                if hour["high"] > high:
+                    high = hour["high"]
+                if hour["low"] < low or low == 0:
+                    low = hour["low"]
+
+            volatility = ((high / low) - 1) * 100
+
+            if volatility > minVolatility:
+                tradeableMarket = {}
+                tradeableMarket["name"] = name
+                tradeableMarket["volatility"] = round(volatility, 2)
+                tradeableMarket["volumeM"] = round(volume / 1000000, 0)
+                tradeableMarkets.append(tradeableMarket)
+
+    df = pd.DataFrame(tradeableMarkets)
+    df.sort_values(by='volatility', inplace=True, ascending=False)    
+
+    return render_template('data.html',  tables=[df.to_html(classes='data', header="true")])
